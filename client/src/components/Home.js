e0b8e0b802b8269a1ecd448a6a9d853fefc210d5
Sherry Carrasco <sc27541@gmail.com>
2019-05-03 17:43:53 -0400

    m

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,15 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
 	return (
+	<div>
 		<h1>Welcome Home</h1>
+		<form method="post" action="http://localhost:3000/">
+			<input type="text" name="title" />
+			<input type="text" name="author" />
+			<input type="text" name="synopsis" />
+			<input type="submit" name="whatever" />
+		</form>
+	</div>
 	);
   }
 }
--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 const app = express();
-
-/*
-Put this in a config / env file, but saving here for now
-*/
 const GoogleApiKey = "AIzaSyCsJFGi3_Ouj4HLOTzcIAR45OJeM0kAths";
-/*
-Example URL:
-https://www.googleapis.com/books/v1/volumes?q=algernon&key=AIzaSyCsJFGi3_Ouj4HLOTzcIAR45OJeM0kAths
-*/
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -19,36 +15,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(routes);
+
 
 // Define API routes here
-
-
-
-/*
-app.get("/googlebooks/search/:query", (req, res) => {
-	res.send("Holla");		
-	let searchUrl = "https://www.googleapis.com/books/v1/volumes?q="+req.params.query+"&key=" + GoogleApiKey;
-	axios.get(searchUrl).then(response => {
-		console.log(response.data);
-		res.send("Holla");		
-		const $ = cheerio.load(response.data);
-		$(".digg-story__content").each((i, element) => {
-			let result = {};
-			result.summary = $(element).find(".js--digg-story__ellipsis-truncate").text();
-			result.link = $(element).find(".digg-story__title-link").attr("href");
-			result.title = $(element).find(".digg-story__title-link").text();
-//			console.log(result);
-			db.Article.create(result).then(dbArticle => {
-				console.log(dbArticle);
-			})
-			.catch(err => {
-				console.log(err);
-			});
-		});
-		res.send("Scrape Complete");
-	});
-});
-*/
 
 
 // Send every other request to the React app
